1e2c1166e18b60c930c5d6c3f88cd726bb7e17b6
DaRKSTeVuS <darkstevus@gmail.com>
2018-12-04 18:47:40 +0100

    correction session.php

--- lib/Session.php
+++ lib/Session.php
@@ -3,6 +3,6 @@ class Session
 {
     public static function is_user($login)
     {
-        return (!empty($_SESSION['login']) && ($_SESSION['login'] == $login));
+        return (!empty($_SESSION['idUtilisateur']) && ($_SESSION['idUtilisateur'] == $login));
     }
 }
