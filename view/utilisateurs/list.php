16d9aad7823011d3f255eb45e0bcfa5dc739f427
Thomas Sanz <t.sanz13@laposte.net>
2018-12-07 14:10:07 +0100

    correction delete image

--- view/utilisateurs/list.php
+++ view/utilisateurs/list.php
@@ -1,6 +1,16 @@
 <?php
-foreach ($tab as $key => $value) {
-    $nomUtilisateur = htmlspecialchars($value->get('nomUtilisateur'));
-    $idUtilisateurURL = rawurlencode($value->get('idUtilisateur'));
-    echo '<p><a href=index.php?controller=utilisateurs&action=read&idUtilisateur=' . $idUtilisateurURL . '>' . $nomUtilisateur . '</a></p>';
+if(Session::is_admin()){
+	echo '<h1>Liste des utilisateurs </h1>';
+	echo '<p><a href=index.php?controller=utilisateurs&action=create>Ajouter un utilisateur</a></p>';        
+
+	foreach ($tab as $key => $value) {
+    	$nomUtilisateur = htmlspecialchars($value->get('nomUtilisateur'));
+    	$idUtilisateurURL = rawurlencode($value->get('idUtilisateur'));
+    	echo '<p><a href=index.php?controller=utilisateurs&action=read&idUtilisateur=' . $idUtilisateurURL . '>' . $nomUtilisateur . '</a></p>';
+	}
+} else{
+	
 }
+
+?>
+
