0495b7c73e851fe7101686485e72939aef8fb285
Stéphane <darkstevus@gmail.com>
2018-12-08 19:03:46 +0100

    Invocation de Saturnin

---
+++ config/Conf_iut_stef.php
@@ -0,0 +1,62 @@
+<?php
+class Conf
+{
+    private static $databases = array(
+        // Le nom d'hote est webinfo a l'IUT
+        // ou localhost sur votre machine
+        'hostname' => 'webinfo',
+        // A l'IUT, vous avez une BDD nommee comme votre login
+        // Sur votre machine, vous devrez creer une BDD
+        'database' => 'combess',
+        // A l'IUT, c'est votre login
+        // Sur votre machine, vous avez surement un compte 'root'
+        'login' => 'combess',
+        // A l'IUT, c'est votre mdp (INE par defaut)
+        // Sur votre machine personelle, vous avez creez ce mdp a l'installation
+        'password' => 'aMeLie00%%',
+        // Prefixe des tables
+        'prefix' => 'cac_',
+    );
+
+    private static $debug = true;
+
+    public static function getDebug()
+    {
+        return self::$debug;
+    }
+
+    public static function getLogin()
+    {
+        //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
+        return self::$databases['login'];
+    }
+
+    public static function getHostname()
+    {
+        return self::$databases['hostname'];
+    }
+
+    public static function getDatabase()
+    {
+        return self::$databases['database'];
+    }
+
+    public static function getPassword()
+    {
+        return self::$databases['password'];
+    }
+
+    public static function getPrefix()
+    {
+        return self::$databases['prefix'];
+    }
+
+    public function getBaseURL()
+    {
+        $currentPath = $_SERVER['PHP_SELF'];
+        $pathInfo = pathinfo($currentPath);
+        $hostName = $_SERVER['HTTP_HOST'];
+        $protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"], 0, 5)) == 'https://' ? 'https://' : 'http://';
+        return $protocol . $hostName . $pathInfo['dirname'] . "/";
+    }
+}
--- sql/patch 0.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `cac_utilisateurs` ADD `nonce` VARCHAR(32) NOT NULL AFTER `idRole`;
