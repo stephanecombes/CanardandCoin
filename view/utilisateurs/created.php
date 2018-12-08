522b120426b860d3fb1577fc8ed8e34757890f6e
Stéphane <darkstevus@gmail.com>
2018-12-08 18:51:36 +0100

    correction de bug de l'inscription

--- view/utilisateurs/created.php
+++ view/utilisateurs/created.php
@@ -1,3 +1,4 @@
 <?php
 echo '<p>L\'Utilisateur a bien été créé(e) !</p>';
-require File::build_path(array('view', 'utilisateurs', 'list.php'));
+echo '<p>Un courriel d\'activation a été envoyé</p>';
+require File::build_path(array('view', 'utilisateurs', 'connect.php'));
