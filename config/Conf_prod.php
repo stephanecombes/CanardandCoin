6681a36a363c06c05f7c926a3502285523bc680e
Stéphane <darkstevus@gmail.com>
2018-12-08 18:53:08 +0100

    ajout fonction conf.pphp

--- config/Conf_prod.php
+++ config/Conf_prod.php
@@ -50,4 +50,13 @@ class Conf
     {
         return self::$databases['prefix'];
     }
+
+    public function getBaseURL()
+    {
+        $currentPath = $_SERVER['PHP_SELF'];
+        $pathInfo = pathinfo($currentPath);
+        $hostName = $_SERVER['HTTP_HOST'];
+        $protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"], 0, 5)) == 'https://' ? 'https://' : 'http://';
+        return $protocol . $hostName . $pathInfo['dirname'] . "/";
+    }
 }
